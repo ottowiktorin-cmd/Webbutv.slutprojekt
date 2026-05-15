b20a931840b76fcde39a2fcab4d03809d12131ef
ottowiktorin-cmd <otto.wiktorin@skola.taby.se>
2026-05-15 23:42:49 +0200

    fixed image

---
+++ java.js
@@ -0,0 +1,16 @@
+// document.getElementById(contact);
+// button(onclick) = onbuttonclicked;
+// function onbuttonclicked(){
+//     if (buttononclicked) {
+//        button1.style.backgroundColor = "grey";
+//        button1.disabled = true;
+//     }  else {
+//        button1.style.backgroundColor = "yellow";
+//        button1.disabled = false;
+//     }
+// }
+
+
+function dark() {
+    document.body.style.background = "black";
+}
